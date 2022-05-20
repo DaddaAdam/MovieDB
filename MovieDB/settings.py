7b9a271d0681e0c2ea0481e02532c7b4e9672223
jadkhafife <khafifejad@gmail.com>
2022-05-20 23:08:58 +0100

    linked backend to frontend @jadkhafife

--- MovieDB/settings.py
+++ MovieDB/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'djoser',
     'api.apps.ApiConfig',
     'imdb.apps.ImdbConfig', 
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'MovieDB.urls'
@@ -95,7 +101,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = credentials.email_adress
 EMAIL_HOST_PASSWORD = credentials.password
 EMAIL_USE_TLS = True
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
