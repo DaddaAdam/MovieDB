66200c3a8dcd28c10dd6aec8d3a9ce7e5af23f61
DaddaAdam <daddaadam@gmail.com>
2022-05-20 18:08:35 +0100

    refactored some imdb api endpoints

--- imdb/urls.py
+++ imdb/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('top250TV/', views.getTop250TV),
-    path('trending-tv/', views.getTrendingTV),
+    path('trending/<str:category>/', views.getTrending),
     path('actor/<str:actor_name>/', views.getActorByName),
-    path('actor/<str:actor_name>/all', views.getCreditsByActor),
-    path('actor/<str:actor_name>/movies', views.getMoviesByActor),
-    path('actor/<str:actor_name>/tv', views.getTvShowsByActor),
+    path('actor/<str:actor_name>/<str:category>/', views.getCreditsByActor),
     path('movies/<str:movie_name>/', views.getMoviesByName)
 ]
--- imdb/views.py
+++ imdb/views.py
@@ -1,4 +1,4 @@
-from operator import itemgetter
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.utils.text import slugify
@@ -20,23 +20,16 @@ def convertActorToId(actor_name):
     return actor_id
  
 @api_view(['GET'])
-def getTop250TV(request): 
-    url = "https://imdb-api.com/en/API/Top250TVs/k_w2ktz9yi"
+def getTrending(request, category):
 
-    headers= {}
-    payload = {}
+    if category not in ('movie', 'tv', 'person'):
+        category = 'all'
 
-    response = requests.request("GET", url, headers=headers, data = payload)
-    return Response(response.json())
-
-
-@api_view(['GET'])
-def getTrendingTV(request):
-    url = f"https://api.themoviedb.org/3/trending/tv/week?api_key={API_KEY}"
+    url = f"https://api.themoviedb.org/3/trending/{category}/week?api_key={API_KEY}"
 
     response = requests.request("GET", url=url, headers={}, data={})
 
-    return Response(response.json())
+    return Response(response.json(), status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
@@ -48,45 +41,22 @@ def getActorByName(request, actor_name):
 
     response = requests.request("GET", url=url, headers={}, data={})
 
-    return Response(response.json())
+    return Response(response.json(), status=status.HTTP_200_OK)
 
 @api_view(['GET'])
-def getCreditsByActor(request, actor_name):
+def getCreditsByActor(request, actor_name, category):
 
     actor_id = convertActorToId(actor_name=actor_name)
 
-    print(actor_id)
+    if category not in ('movie', 'tv'):
+        category = 'combined'
 
-    url = f"https://api.themoviedb.org/3/person/{actor_id}/combined_credits?sort_by=popularity.desc&api_key={API_KEY}&language=en-US"
 
-    response = requests.request("GET", url=url, headers={}, data={})
-
-    return Response(response.json())
-
-@api_view(['GET'])
-def getMoviesByActor(request, actor_name):
-
-    actor_id = convertActorToId(actor_name)
-
-    url = f"https://api.themoviedb.org/3/person/{actor_id}/movie_credits?sort_by=popularity.desc&api_key={API_KEY}&language=en-US"
+    url = f"https://api.themoviedb.org/3/person/{actor_id}/{category}_credits?sort_by=popularity.desc&api_key={API_KEY}&language=en-US"
 
     response = requests.request("GET", url=url, headers={}, data={})
 
-    return Response(response.json())
-
-
-@api_view(['GET'])
-def getTvShowsByActor(request, actor_name):
-
-    actor_id = convertActorToId(actor_name)
-
-    url = f"https://api.themoviedb.org/3/person/{actor_id}/tv_credits?sort_by=popularity.desc&api_key={API_KEY}&language=en-US"
-
-    response = requests.request("GET", url=url, headers={}, data={})
-
-    print(response.json()["cast"][0]["popularity"])
-
-    return Response(response.json())
+    return Response(response.json(), status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def getMoviesByName(request, movie_name):
@@ -96,4 +66,4 @@ def getMoviesByName(request, movie_name):
 
     response = requests.request("GET", url=url, headers={}, data={})
 
-    return Response(response.json())
+    return Response(response.json(), status=status.HTTP_200_OK)
